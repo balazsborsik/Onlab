92b6e1f41d520595381d4348639c7bc8f1b5c53c
Borsik Balázs <baborsik@gmail.com>
2025-05-18 20:21:15 +0200

    big table

--- current_results.cpp
+++ current_results.cpp
@@ -49,6 +49,38 @@ bool is_number(const std::string& s)
     return !s.empty() && it == s.end();
 }
 
+void print_in_latexformat(const vector<vector<int>>& res, int size){
+    stringstream str;
+    ofstream outfile ("results_latex.txt");
+    string start=R"(\begin{center}
+\renewcommand{\arraystretch}{1.2}
+\setlength{\tabcolsep}{3pt}
+\scriptsize
+\resizebox{\textwidth}{!}{
+\begin{tabular}{r|*{39}{c}} % 34 oszlop n=2..39
+$m \backslash n$ & 2 & 3 & 4 & 5 & 6 & 7 & 8 & 9 & 10 & 11 & 12 & 13 & 14 & 15 & 16 & 17 & 18 & 19 & 20 & 21 & 22 & 23 & 24 & 25 & 26 & 27 & 28 & 29 & 30 & 31 & 32 & 33 & 34 & 35 & 36 & 37 & 38 & 39 & 40 \\
+\hline)";
+string end= R"(\end{tabular}
+}
+\vspace{1em}
+\captionof{table}{$z_2(m,n)$}
+\end{center})";
+    outfile<<start<<endl;
+    for(int i=0;i<size;i++){
+        outfile<<i+2;
+        for(int j=0;j<size;j++){
+            int val=res[j][i];
+            if(val)
+                outfile<<" & "<<val;
+            else
+                outfile<<" &  ";
+        }
+        outfile<<" \\\\"<<endl;
+    }
+    outfile<<end;
+    outfile.close();
+}
+
 int main(int argc, char* argv[]){
     if (argc !=1 && (argc != 3 || !is_number(argv[1]) || !is_number(argv[2]))) {
         cerr << "Usage: " << argv[0] << " [<min size> <max size>]" << endl;
@@ -86,4 +118,7 @@ int main(int argc, char* argv[]){
         outfile<<endl;
     }
     outfile.close();
+    print_in_latexformat(res,size);
+
+    
 }
